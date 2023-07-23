8dd3d77bceb66e181f339008f23e15a47ef4e13f
AkshayS734 <manasshukla876@gmail.com>
2023-07-23 18:58:32 +0530

    Commit 1

---
+++ index.js
@@ -0,0 +1,19 @@
+var i=Math.random()*6 +1;
+i=Math.floor(i);
+var j= Math.random()*6 +1;
+j=Math.floor(j);
+var n=document.querySelectorAll("img");
+var firstImage='images/dice'+i+'.png';
+var secondImage='images/dice'+j+'.png';
+n[0].setAttribute("src",firstImage);
+n[1].setAttribute("src",secondImage);
+var x=document.querySelector("h1");
+if(j>i){
+    x.innerText="Player 2 wins";
+}
+else if(j==i){
+    x.innerText="Draw"
+}
+else{
+    x.innerText="Player 1 Wins";
+}
