525910e09c6e40213fe802d37734f9a19540883f
AkshayS734 <manasshukla876@gmail.com>
2023-07-31 22:10:49 +0530

    Added refresh button

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,22 @@
-var i=Math.random()*6 +1;
-i=Math.floor(i);
-var j= Math.random()*6 +1;
-j=Math.floor(j);
-var n=document.querySelectorAll("img");
-var firstImage='assets/images/dice'+i+'.png';
-var secondImage='assets/images/dice'+j+'.png';
-n[0].setAttribute("src",firstImage);
-n[1].setAttribute("src",secondImage);
-var x=document.querySelector("h1");
-if(j>i){
-    x.innerText="Player 2 wins";
-}
-else if(j==i){
-    x.innerText="Draw"
-}
-else{
-    x.innerText="Player 1 Wins";
-}
+var refbutton=document.getElementById("refbtn");
+refbutton.addEventListener("click",function(){
+    var i=Math.random()*6 +1;
+    i=Math.floor(i);
+    var j= Math.random()*6 +1;
+    j=Math.floor(j);
+    var n=document.querySelectorAll("img");
+    var firstImage='assets/images/dice'+i+'.png';
+    var secondImage='assets/images/dice'+j+'.png';
+    n[0].setAttribute("src",firstImage);
+    n[1].setAttribute("src",secondImage);
+    var x=document.querySelector(".Result");
+    if(j>i){
+        x.innerText="Player 2 wins";
+    }
+    else if(j==i){
+        x.innerText="Draw"
+    }
+    else{
+        x.innerText="Player 1 Wins";
+    }
+})
